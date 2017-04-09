8b1a7fae55899cd562f64ba09460f50ab568d809
pranavgoel25 <pranavgoel_25@hotmail.com>
2017-04-09 19:13:30 +0530

    Add GET handler

--- server/server.py
+++ server/server.py
@@ -10,9 +10,12 @@ if len(sys.argv) < 2:
 
 PORT = int(sys.argv[1])
 
+
 class HTTPCacheRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def send_head(self):
+        print 'in f1'
         if self.command != "POST" and self.headers.get('If-Modified-Since', None):
+            print 'in if of f1'
             filename = self.path.strip("/")
             if os.path.isfile(filename):
                 a = time.strptime(time.ctime(os.path.getmtime(filename)), "%a %b %d %H:%M:%S %Y")
@@ -21,18 +24,32 @@ class HTTPCacheRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
                     self.send_response(304)
                     self.end_headers()
                     return None
+        print 'going out of f1'
         return SimpleHTTPServer.SimpleHTTPRequestHandler.send_head(self)
 
     def end_headers(self):
+        print 'in f2'
         self.send_header('Cache-control', 'must-revalidate')
         SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
+        print 'going out of f2'
 
     def do_POST(self):
+        print 'in f3'
         self.send_response(200)
         self.send_header('Cache-control', 'no-cache')
         SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
+        print 'going out of f3'
+
+    def do_GET(self):
+        print 'in f4'
+        self.send_response(200)
+        self.send_header("Content-type", 'text/html')
+        SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
+        self.wfile.write("<html><head><title>Test</title></head></html>")
+        print 'going out of f4'
 
 s = SocketServer.ThreadingTCPServer(("", PORT), HTTPCacheRequestHandler)
 s.allow_reuse_address = True
 print "Serving on port", PORT
+print 'going to serve_forever()'
 s.serve_forever()
