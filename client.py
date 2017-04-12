992ff92eca64658eda8a727ce1d30aa79bc6eaf7
FleetAdmiral <pranavnair97@gmail.com>
2017-04-12 15:31:12 +0530

    Nice

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ if len(sys.argv) < 4:
     print "Example: python client.py 19990-19999 20000 20010"
     raise SystemExit
 
-CLIENT_PORT = sys.argv[1]
+CLIENT_PORTA = sys.argv[1]
 PROXY_PORT = sys.argv[2]
 SERVER_PORT = sys.argv[3]
 
@@ -17,7 +17,7 @@ D = {0: "GET", 1: "GET"}
 while True:
     filename = "%d.data" % (int(random.random() * 2) + 1)
     METHOD = D[int(random.random() * len(D))]
-    CLIENT_PORT = str(random.randint(int(CLIENT_PORT), int(CLIENT_PORT) + 6))
+    CLIENT_PORT = str(random.randint(int(CLIENT_PORTA), int(CLIENT_PORTA) + 6))
 
     print 'sending request - ', "curl --request GET --proxy 127.0.0.1:%s --local-port %s geeksforgeeks.org" % (
         PROXY_PORT, CLIENT_PORT)
